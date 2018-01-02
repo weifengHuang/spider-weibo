4d5bfbcaf728a4eafdb68dcb6ed33d22ecda1f1d
DESKTOP-SRN6JR7\hwf <463815567@qq.com>
2018-01-02 23:12:37 +0800

    fix: 修复抓取，目前抓到253个

--- util/createBrowser.js
+++ util/createBrowser.js
@@ -3,7 +3,7 @@ const createBrowser = async () => {
   const browser = await puppeteer.launch({
     executablePath: '../node_modules/puppeteer/local/chrome-win32/chrome',
     headless: false,
-    userDataDir: '../weibo'
+    userDataDir: 'weibo'
   })
   return browser
 }
