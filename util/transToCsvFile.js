323436acca413e77fa53c181a93f144227e5c8c6
DESKTOP-SRN6JR7\hwf <463815567@qq.com>
2018-01-06 10:17:40 +0800

    fix: 修改默认文件名

--- util/transToCsvFile.js
+++ util/transToCsvFile.js
@@ -1,10 +1,10 @@
 var json2csv = require('json2csv')
 var fs = require('fs')
-const transToCsvFile = (data, option) => {
+const transToCsvFile = (data, option = {}) => {
   let {fields, fieldNames, path} = option
   fields = fields || ['name', 'href', 'address']
   fieldNames = fieldNames || ['名称', '链接', '地址']
-  path = path || new Date().getTime()
+  path = path || `${new Date().getTime()}.csv`
   // const fields = ['name', 'href', 'address']
   // const fieldNames = ['名称', '链接', '地址']
   const csv = json2csv({data, fields, fieldNames})
